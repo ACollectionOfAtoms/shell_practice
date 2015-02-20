6236022212804b0681f2f3be1a97785350e5e171
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-02-20 14:54:55 -0600

    Still working out bugs in factorial.sh

--- factorial.sh
+++ factorial.sh
@@ -1,22 +1,20 @@
 #bin/zsh
-
 echo "Supply a number: \c"
 read num
 counter=$num
+ints='^[0-9]+$'
 
-if [[ $num="1" ]];then
-	echo "The factorial of your number is 1!"; exit 1
-elif
-	[[ $num="1" ]];then
-	echo "The factorial of your number is 1!"; exit 1
-else
-	while [ $counter > "0" ];do
+#if [[ "$num" = "" ]] || ! [[ "$num" =~ $ints ]];then
+#	echo "Error: You failed to supply a number. Good day!" >&2; exit 1
+#elif [[ $num==1 ]] || [[ $num==0 ]];then
+#	echo "The factorial of your number is 1!"; exit 1
+#fi
+
+if [[ "$num" > "1" ]];then
+	while ! [ "$counter" = 0 ];do
 		counter=`expr $counter - 1`
+		echo $num
 		num=`expr $num \* $counter`
 	done
-fi
-
-if [[ $counter=0 ]];then
 	echo "The factorial of your num num number is $num!"; exit 1
 fi
-
