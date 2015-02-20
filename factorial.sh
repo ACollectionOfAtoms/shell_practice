b4046d40b49da95d54509c20766ca52f9fd05151
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-02-20 15:29:07 -0600

    Working factorial.sh script added

--- factorial.sh
+++ factorial.sh
@@ -3,18 +3,17 @@ echo "Supply a number: \c"
 read num
 counter=$num
 ints='^[0-9]+$'
-
-#if [[ "$num" = "" ]] || ! [[ "$num" =~ $ints ]];then
-#	echo "Error: You failed to supply a number. Good day!" >&2; exit 1
-#elif [[ $num==1 ]] || [[ $num==0 ]];then
-#	echo "The factorial of your number is 1!"; exit 1
-#fi
-
 if [[ "$num" > "1" ]];then
 	while ! [ "$counter" = 0 ];do
 		counter=`expr $counter - 1`
-		echo $num
 		num=`expr $num \* $counter`
+		if [[ "$counter" = 1 ]];then
+			echo "The factorial of your number is $num!"; exit 1
+		fi
 	done
-	echo "The factorial of your num num number is $num!"; exit 1
+elif [[ "$num" = 1 ]] || [[ "$num" = 0 ]];then
+	echo "The factorial of your number is 1!"; exit 1
+elif [[ "$num" = "" ]] || ! [[ "$num" =~ $ints ]];then
+	echo "Error: You must supply a number! EXITING!" >&2; exit 1
 fi
+
